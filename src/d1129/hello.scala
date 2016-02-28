cddca3a64779b16a2e942c12c0f67b81de69e4ec
Hanuk Lee <xohzzwn6kcj9@gmail.com>
2016-02-28 15:00:45 +0900

    commit test

--- src/d1129/hello.scala
+++ src/d1129/hello.scala
@@ -1,8 +1,23 @@
 package d1129
 
 object hello {
-  def main(args: Array[String]) {
-    println("test")
+  def main(args: Array[String]): Unit = {
+
+    //import scala.math.BigInt._
+
+    def gcd(a: BigInt, b: BigInt): BigInt = if(b == BigInt(0)) a else gcd(b, a%b)
+    def lcm(a: BigInt, b: BigInt): BigInt = (a * b) / gcd(a, b)
+    //val n: BigInt = 20
+    //val ans = (1 to n).reduce(lcm)
+    //val ans = (1 to n).reduce((a, b) => a * b / a.gcd(b))
+    val ans = (1 to 20).map(BigInt(_)).reduce(lcm)
+    //val ans = (BigInt(1) to BigInt(20)).reduce(lcm)
+
+    println(ans)
+    //println((1 to 20).map(BigInt(_)).reduce((a, b) => a * b / a.gcd(b)))
+
+    val ans2 = (BigInt(1) to BigInt(2000000)).filter(_.isProbablePrime(10)).sum
+    println(ans2)
   }
 
 }
